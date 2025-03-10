e05bceaa8a91158eeac45de86a9634b9536ad741
hgajjar27 <hgajjar@depaul.edu>
2025-03-09 19:28:38 -0500

    FileRetrievalEngineService.java

--- app-java/src/main/java/csc435/app/FileRetrievalEngineService.java
+++ app-java/src/main/java/csc435/app/FileRetrievalEngineService.java
@@ -1,52 +1,43 @@
 package csc435.app;
 
+import java.util.ArrayList;
 import io.grpc.stub.StreamObserver;
+import io.grpc.stub.StreamObserver;
+import com.google.protobuf.Empty;
 
+// gRPC Service Implementation
 public class FileRetrievalEngineService extends FileRetrievalEngineGrpc.FileRetrievalEngineImplBase {
     private IndexStore store;
-    // TO-DO keep track of the client IDs
-    
+
+    // Constructor to initialize the index store
     public FileRetrievalEngineService(IndexStore store) {
         this.store = store;
     }
 
-    @Override
-    public void register(com.google.protobuf.Empty request, StreamObserver<RegisterRep> responseObserver) {
-        responseObserver.onNext(doRegister());
+    // Method to register the client and return a client ID
+    public void register(Empty request, StreamObserver<RegisterRep> responseObserver) {
+        long clientId = store.putDocument("new_client");
+        responseObserver.onNext(RegisterRep.newBuilder().setClientId(clientId).build());
         responseObserver.onCompleted();
     }
 
-    @Override
+    // Method to process the index request
     public void computeIndex(IndexReq request, StreamObserver<IndexRep> responseObserver) {
-        responseObserver.onNext(doIndex(request));
+        store.updateIndex(request.getDocumentNumber(), request.getWordFrequenciesMap());
+        responseObserver.onNext(IndexRep.newBuilder().setStatus("OK").build());
         responseObserver.onCompleted();
     }
 
-    @Override
+    // Method to process search request
     public void computeSearch(SearchReq request, StreamObserver<SearchRep> responseObserver) {
-        responseObserver.onNext(doSearch(request));
+        ArrayList<DocFreqPair> results = store.lookupIndex(request.getSearchTerm());
+        SearchRep.Builder builder = SearchRep.newBuilder();
+        for (DocFreqPair pair : results) {
+            builder.addDocuments(DocFreqPair.newBuilder()
+                    .setDocumentNumber(pair.documentNumber)
+                    .setWordFrequency(pair.wordFrequency).build());
+        }
+        responseObserver.onNext(builder.build());
         responseObserver.onCompleted();
     }
-
-    private RegisterRep doRegister() {
-        // TO-DO generate a client ID
-        //       return the client ID as a RegisterRep reply
-
-        return RegisterRep.newBuilder().build();
-    }
-
-    private IndexRep doIndex(IndexReq request) {
-        // TO-DO update global index with temporary index received from the request
-        // TO-DO send an OK message as the reply
-
-        return IndexRep.newBuilder().build();
-    }
-
-    private SearchRep doSearch(SearchReq request) {
-        // TO-DO do lookup over the global index given the search term from the request
-        // TO-DO send the results as the reply message
-
-        return SearchRep.newBuilder().build();
-    }
-
 }
