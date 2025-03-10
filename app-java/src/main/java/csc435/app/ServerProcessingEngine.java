88c3b105f1f5b72fec7513dce3abea36f3bb604d
hgajjar27 <hgajjar@depaul.edu>
2025-03-09 19:33:26 -0500

    ServerProcessingEngine.java

--- app-java/src/main/java/csc435/app/ServerProcessingEngine.java
+++ app-java/src/main/java/csc435/app/ServerProcessingEngine.java
@@ -1,27 +1,30 @@
 package csc435.app;
 
-import io.grpc.Grpc;
-import io.grpc.InsecureServerCredentials;
 import io.grpc.ServerBuilder;
+import io.grpc.Server;
 
 public class ServerProcessingEngine {
     private IndexStore store;
-    // TO-DO keep track of the RPCServerWorker object
-    // TO-DO keep track of the gRPC server thread
 
     public ServerProcessingEngine(IndexStore store) {
         this.store = store;
     }
 
-    // TO-DO create and start the gRPC Server
     public void initialize(int serverPort) {
-        // TO-DO create the RPCServerWorker object
-        // TO-DO create and start the gRPC server thread that runs in the context of the RPCServerWorker object
+        try {
+            Server server = ServerBuilder.forPort(serverPort)
+                .addService(new FileRetrievalEngineService(store))
+                .build();
+            server.start();
+            System.out.println("Server started...");
+            server.awaitTermination();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    // TO-DO shutdown the gRPC Server
+    // Method to shutdown the server
     public void shutdown() {
-        // TO-DO call shutdown on the RPCServerWorker object
-        // TO-DO join the gRPC server thread
+        // No shutdown logic needed for this 
     }
 }
